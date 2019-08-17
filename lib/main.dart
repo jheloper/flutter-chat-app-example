c749360350d71448af30630899b5f4fffa39927a
Joonghyeon Kim <jheloper9102@gmail.com>
2019-08-17 22:53:21 +0900

    메세지를 입력할 수 있는 텍스트필드 위젯 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,51 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      new MaterialApp(
-        title: "FriendlyChat",
-        home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text("FriendlyChat"),
-          ),
-        )
-      )
+      new FriendlyChatApp()
   );
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "FriendlyChat",
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+
+  final TextEditingController _textController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("FriendlyChat"),),
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: new InputDecoration.collapsed(hintText: "Send a Message"),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
 }
